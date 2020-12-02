81735952954a4aa717447a7fe80b2421e28667b6
mokulling <72229978+mokulling@users.noreply.github.com>
2020-12-02 11:30:06 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+var time = document.getElementById('time')
+var question = document.getElementById('question')
+var highscore = document.getElementById('highscore')
+var start = document.getElementById('start')
+var time = document.createElement('p')
+
