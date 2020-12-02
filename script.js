89d1f383acea782b596939202cb522ee49bde150
mokulling <72229978+mokulling@users.noreply.github.com>
2020-12-02 11:39:33 -0600

    created timer

--- script.js
+++ script.js
@@ -3,4 +3,25 @@ var question = document.getElementById('question')
 var highscore = document.getElementById('highscore')
 var start = document.getElementById('start')
 var time = document.createElement('p')
+var count = 15
+var timer = setInterval(function() {
+    console.log(count);
+    count--;
+    if(count===0){
+        stopInterval()
+    }
+}, 1000);
 
+var stopInterval = function() {
+    console.log('time\'s up!');
+    clearInterval(timer);
+}
+
+
+
+
+startButton()
+
+function startButton() {
+
+}
